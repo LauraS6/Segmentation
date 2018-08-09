00c178352cb968daaf68a305aaa2d4c31da1d771
LauraS6 <42166817+LauraS6@users.noreply.github.com>
2018-08-09 16:07:50 +0200

    Update SuperpositionFoieCavite.py

--- SuperpositionFoieCavite.py
+++ SuperpositionFoieCavite.py
@@ -3,6 +3,7 @@ import unittest
 import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
 import logging
+import Elastix
 
 #
 # SuperpositionFoieCavite
@@ -19,7 +20,8 @@ class SuperpositionFoieCavite(ScriptedLoadableModule):
     self.parent.categories = ["SUPERPOSITION donneur / receveur"]
     self.parent.dependencies = []
     self.parent.contributors = ["Laura Seimpere"]
-    self.parent.helpText = """
+    self.parent.helpText = u"""
+    Superposition du foie segment\u00e9 et de la cavit\u00e9 abdominale segment\u00e9e par les modules pr\u00e9c\u00e9dents et apr\u00e8s recalage rigide 3D entre les deux volumes charg\u00e9s.
 """
     self.parent.helpText += self.getDefaultModuleDocumentationLink()
     self.parent.acknowledgementText = """
@@ -30,15 +32,11 @@ class SuperpositionFoieCavite(ScriptedLoadableModule):
 #
 
 class SuperpositionFoieCaviteWidget(ScriptedLoadableModuleWidget):
-  """Uses ScriptedLoadableModuleWidget base class, available at:
-  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
-  """
-
   def setup(self):
     ScriptedLoadableModuleWidget.setup(self)
 
 		#########
-    # Step 1 : Charger images
+    # Step 1 : Charger les images
 		#########
     stepsCollapsibleButton = ctk.ctkCollapsibleButton()
     stepsCollapsibleButton.text = u"SUPERPOSITION cavit\u00e9 du receveur / foie segment\u00e9 du donneur"
@@ -47,22 +45,29 @@ class SuperpositionFoieCaviteWidget(ScriptedLoadableModuleWidget):
 
 					# Chargement avec dossier DICOM
 					# Bouton de chargement par DICOM mis sur True
-    self.loadcavite = qt.QRadioButton("Charger la CAVITE")
+    self.loadcavite = qt.QRadioButton("Charger la CAVITE SEGMENTEE")
     self.loadcavite.checked = True
     stepsFormLayout.addRow(self.loadcavite)
-    self.VolumeNode1 = None
-    self.VolumeNode2 = None
+    self.VolumeNodecavite = None
+    self.VolumeNodefoie = None
 
 					# Chargement avec dossier DICOM
 					# Bouton de chargement par DICOM mis sur True
     self.loadfoie = qt.QRadioButton("Charger le FOIE SEGMENTE")
     stepsFormLayout.addRow(self.loadfoie)
     
+					# Apply Button
+    self.applyButton = qt.QPushButton(u"Ex\u00e9cuter")
+    self.applyButton.toolTip = u"Ex\u00e9cuter l\u00b4algorithme."
+    self.applyButton.enabled = True
+    self.applyButton.setMaximumWidth(350)
+    stepsFormLayout.addRow(self.applyButton)
+    
 					# Espace pour une meilleure visibilite dans l'affichage
     self.inputDistLabel = qt.QLabel("")
     stepsFormLayout.addRow(self.inputDistLabel)
     
-    					# Input volume selector
+    			# Input volume selector
     self.inputSelector1 = slicer.qMRMLNodeComboBox()
     self.inputSelector1.nodeTypes = ["vtkMRMLScalarVolumeNode"]
     self.inputSelector1.selectNodeUponCreation = True
@@ -74,9 +79,9 @@ class SuperpositionFoieCaviteWidget(ScriptedLoadableModuleWidget):
     self.inputSelector1.setMRMLScene( slicer.mrmlScene)
     self.inputSelector1.setToolTip(u"Choisir l\u00b4entr\u00e9e de l\u00b4algorithme." )
     self.inputSelector1.setMaximumWidth(250)
-    stepsFormLayout.addRow(u"  Volume d\u00b4entr\u00e9e de la cavit\u00e9 :                        ", self.inputSelector1)
+    stepsFormLayout.addRow(u"  Volume de la cavit\u00e9 segment\u00e9e :                        ", self.inputSelector1)
     
-    					# Input volume selector
+    			# Input volume selector
     self.inputSelector2 = slicer.qMRMLNodeComboBox()
     self.inputSelector2.nodeTypes = ["vtkMRMLScalarVolumeNode"]
     self.inputSelector2.selectNodeUponCreation = True
@@ -88,20 +93,22 @@ class SuperpositionFoieCaviteWidget(ScriptedLoadableModuleWidget):
     self.inputSelector2.setMRMLScene( slicer.mrmlScene )
     self.inputSelector2.setToolTip(u"Choisir l\u00b4entr\u00e9e de l\u00b4algorithme." )
     self.inputSelector2.setMaximumWidth(250)
-    stepsFormLayout.addRow(u"  Volume d\u00b4entr\u00e9e du foie :                        ", self.inputSelector2)
+    stepsFormLayout.addRow(u"  Volume du foie segment\u00e9 apr\u00e8s recalage :                        ", self.inputSelector2)
     
-    # Apply Button
-    self.applyButton = qt.QPushButton(u"Ex\u00e9cuter")
-    self.applyButton.toolTip = u"Ex\u00e9cuter l\u00b4algorithme."
-    self.applyButton.enabled = True
-    stepsFormLayout.addRow(self.applyButton)
-
-    # Connections
+					# Superposition Button
+    self.superposButton = qt.QPushButton(u"Superposer les segmentations")
+    self.superposButton.toolTip = u"Ex\u00e9cuter l\u00b4algorithme."
+    self.superposButton.enabled = True
+    self.superposButton.setMaximumWidth(350)
+    stepsFormLayout.addRow(self.superposButton)
+    
+					# Connections
     self.loadcavite.connect("clicked(bool)", self.onSelect)
     self.loadfoie.connect("clicked(bool)", self.onSelect)
     self.applyButton.connect('clicked(bool)', self.onApplyButton)
-
-    # Add vertical spacer
+    self.superposButton.connect('clicked(bool)', self.onSuperposButton)
+    
+					# Add vertical spacer
     self.layout.addStretch(1)
 
 
@@ -110,26 +117,35 @@ class SuperpositionFoieCaviteWidget(ScriptedLoadableModuleWidget):
 
 
   def onSelect(self):
+					# Recherche du fichier NIFTI pour charger la cavite segmentee
     if self.loadcavite.checked:
-      self.VolumeNode1 = os.path.join(os.path.expanduser("~"),"Documents/TroisDSlicer/Imageasuperposer/")
+      self.VolumeNodecavite = os.path.join(os.path.expanduser("~"),"Documents/TroisDSlicer/Imageasuperposer/")
       slicer.util.openAddVolumeDialog()
-      self.masterVolumeNode = self.VolumeNode1
-      
+      self.masterVolumeNode = self.VolumeNodecavite
+					
+					# Recherche du fichier NIFTI pour charger le foie segmente
     if self.loadfoie.checked:
-      self.VolumeNode2 = os.path.join(os.path.expanduser("~"),"Documents/TroisDSlicer/Imageasuperposer/")
+      self.VolumeNodefoie = os.path.join(os.path.expanduser("~"),"Documents/TroisDSlicer/Imageasuperposer/")
       slicer.util.openAddVolumeDialog()
-      self.masterVolumeNode = self.VolumeNode2
+      self.masterVolumeNode = self.VolumeNodefoie
     self.applyButton.enabled = self.masterVolumeNode
+    
+    
+  def onApplyButton(self):   
+					# Ouverture et traitement sur le module 3D Slicer General Registration (Elastix)
+    reformatModuleWidget = slicer.modules.elastix.createNewWidgetRepresentation() 
+    reformatModuleWidget.setMRMLScene(slicer.app.mrmlScene())
+    reformatModuleWidget.show()
 
 
-  def onApplyButton(self):
+  def onSuperposButton(self):
     logic = SuperpositionFoieCaviteLogic()
     lm = slicer.app.layoutManager()
     lm.setLayout(slicer.vtkMRMLLayoutNode.SlicerLayoutFourUpView)
-    self.VolumeNode1 = self.inputSelector1.currentNode()
-    self.VolumeNode2 = self.inputSelector2.currentNode()
+    VolumeNodecavite = self.inputSelector1.currentNode()
+    VolumeNodefoie = self.inputSelector2.currentNode()
     
-            			## Affichage 
+					# Affichage de la superposition entre le foie segmente et recale et la cavite abdominale segmentee
     lm = slicer.app.layoutManager()
     for sliceViewName in lm.sliceViewNames():
       sliceWidget = lm.sliceWidget(sliceViewName)
@@ -137,12 +153,18 @@ class SuperpositionFoieCaviteWidget(ScriptedLoadableModuleWidget):
       sliceNode = view.mrmlSliceNode()
       sliceLogic = slicer.app.applicationLogic().GetSliceLogic(sliceNode)
       compositeNode = sliceLogic.GetSliceCompositeNode()
-					# Setup background volume
-      compositeNode.SetBackgroundVolumeID(self.VolumeNode1.GetID())
-					# Setup foreground volume
-      compositeNode.SetForegroundVolumeID(self.VolumeNode2.GetID())
-					# Changer l'opacite
-      compositeNode.SetForegroundOpacity(0.7)
+					# Volume du fond = volume du foie segmente puis recale par le module General Registration 
+      compositeNode.SetBackgroundVolumeID(VolumeNodefoie.GetID())
+					# Volume au premier plan = volume de la cavite abdominale segmentee
+      compositeNode.SetForegroundVolumeID(VolumeNodecavite.GetID())
+					# Changer l'opacite du volume au premier plan
+      compositeNode.SetForegroundOpacity(0.3)
+
+					# Sauvegarde du volume du foie segmente puis recale au format NIFTI
+    slicer.util.saveNode(VolumeNodefoie, '~\Documents\TroisDSlicer\FoieRecalage.nii')
+					# Sauvegarde des elements de la scene dans un dossier
+    filename = os.path.join(os.path.expanduser("~"),'\Documents\TroisDSlicer\FoieRecalageData')
+    slicer.util.saveScene(filename)
 
 
 #
